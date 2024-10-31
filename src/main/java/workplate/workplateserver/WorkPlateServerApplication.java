927a44e684d26fa109d1b62c0356781cfac9b14b
박지혁 <pjh5365@naver.com>
2024-10-31 10:44:41 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/workplate/workplateserver/WorkPlateServerApplication.java
@@ -0,0 +1,13 @@
+package workplate.workplateserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPlateServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPlateServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/workplate/workplateserver/WorkPlateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package workplate.workplateserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPlateServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
