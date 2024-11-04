e1d15c0ae4f388dd0a1ee2247509547b7d5c7173
박지혁 <pjh5365@naver.com>
2024-11-04 09:45:02 +0900

    Feat: 회원 상세정보 등록 메서드 추가
    Related to: #1

--- src/main/java/workplate/workplateserver/auth/service/MemberService.java
+++ src/main/java/workplate/workplateserver/auth/service/MemberService.java
@@ -1,12 +1,19 @@
 package workplate.workplateserver.auth.service;
 
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import workplate.workplateserver.auth.domain.dto.request.JoinRequest;
+import workplate.workplateserver.auth.domain.dto.request.MemberDetailRequest;
 import workplate.workplateserver.auth.domain.entity.Member;
+import workplate.workplateserver.auth.domain.entity.MemberDetail;
+import workplate.workplateserver.auth.repository.MemberDetailRepository;
 import workplate.workplateserver.auth.repository.MemberRepository;
+import workplate.workplateserver.common.CommonService;
 
 /**
  * 회원 서비스
@@ -19,6 +26,8 @@ import workplate.workplateserver.auth.repository.MemberRepository;
 public class MemberService {
 
 	private final MemberRepository memberRepository;
+	private final MemberDetailRepository memberDetailRepository;
+	private final CommonService commonService;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	/**
@@ -33,5 +42,20 @@ public class MemberService {
 
 		memberRepository.save(Member.toEntity(joinRequest.getUsername(), joinRequest.getName(),
 				bCryptPasswordEncoder.encode(joinRequest.getPassword())));
+	}
+
+	/**
+	 * 회원 상세정보 등록 메서드
+	 *
+	 * @param request 등록 요청
+	 */
+	public void saveDetails(MemberDetailRequest request) {
+		Member member = commonService.findByUsername(request.getUsername(), true);
+
+		MemberDetail memberDetail = MemberDetail.toEntity(member, request.getAge(), request.getExperience(),
+				request.getPhysicalStatus(),
+				request.getPreference());
+
+		memberDetailRepository.save(memberDetail);
 	}
 }
