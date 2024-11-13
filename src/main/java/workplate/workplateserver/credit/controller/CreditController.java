cccb303538b712368d1ef46872e52566c43796b2
박지혁 <pjh5365@naver.com>
2024-11-13 18:37:27 +0900

    Feat: 크레딧 컨트롤러 작성
    Related to: #5

---
+++ src/main/java/workplate/workplateserver/credit/controller/CreditController.java
@@ -0,0 +1,47 @@
+package workplate.workplateserver.credit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import workplate.workplateserver.common.ApiResponse;
+import workplate.workplateserver.credit.domain.dto.CreditRequest;
+import workplate.workplateserver.credit.domain.dto.CreditResponse;
+import workplate.workplateserver.credit.service.CreditService;
+
+/**
+ * 크레딧 Controller
+ *
+ * @author : parkjihyeok
+ * @since : 2024/11/13
+ */
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class CreditController {
+
+	private final CreditService creditService;
+
+	@GetMapping("/credits/{username}")
+	public ApiResponse<CreditResponse> findByUsername(@PathVariable String username) {
+		CreditResponse response = creditService.findCredit(username);
+
+		return ApiResponse.success(response);
+	}
+
+	@PatchMapping("/credits/plus")
+	public ApiResponse<String> plusCredit(@RequestBody CreditRequest request) {
+		creditService.plusCredit(request.getUsername(), request.getBalance());
+		return ApiResponse.success(null);
+	}
+
+	@PatchMapping("/credits/minus")
+	public ApiResponse<String> minusCredit(@RequestBody CreditRequest request) {
+		creditService.minusCredit(request.getUsername(), request.getBalance());
+		return ApiResponse.success(null);
+	}
+}
